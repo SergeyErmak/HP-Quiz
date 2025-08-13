86cd149961f6758677e5aa35cd80026769622969
SergeyErmak <36698778+SergeyErmak@users.noreply.github.com>
2025-08-13 15:18:11 +0200

    HPQuiz

---
+++ src/components/MagicSkillsQuiz.jsx
@@ -0,0 +1,134 @@
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import questionsData from '../question.json';
+
+export default function MagicSkillsQuiz() {
+	const [quizStarted, setQuizStarted] = useState(false); //Quiz Start
+	const [currentQuestion, setCurrentQuestion] = useState(0); //Current question
+	const [score, setScore] = useState(0);
+	const [selected, setSelected] = useState(null); //Selected question
+	const [answers, setAnswers] = useState([]); //Array of answers
+	const [showResult, setShowResult] = useState(false); //Result
+
+	//Delete data from localStorage after refreshing web-page
+	useEffect(() => {
+		localStorage.removeItem('hpQuizScore');
+		setScore(0);
+	}, []);
+
+	//Render questions from API in json data
+	useEffect(() => {
+		if (quizStarted) {
+			const shuffled = [...questionsData].map((q) => ({
+				...q,
+				answers: q.answers
+					.map((a, i) => ({ text: a, index: i }))
+					.sort(() => Math.random() - 0.5),
+			}));
+			setAnswers(shuffled);
+		}
+	}, [quizStarted]);
+
+	//Function for handling events with answers and save right answers in localStorage
+	const handleAnswer = (optionIndex) => {
+		setSelected(optionIndex);
+
+		if (optionIndex === answers[currentQuestion].correct) {
+			setScore((prev) => {
+				const newScore = prev + 1;
+				localStorage.setItem('hpQuizScore', newScore);
+				return newScore;
+			});
+		}
+
+		setTimeout(() => {
+			setSelected(null);
+			if (currentQuestion + 1 < answers.length) {
+				setCurrentQuestion((prev) => prev + 1);
+			} else {
+				setShowResult(true);
+			}
+		}, 1000);
+	};
+
+	//Function for reseting localStorage and quiz
+	const resetQuiz = () => {
+		setQuizStarted(false);
+		setCurrentQuestion(0);
+		setSelected(null);
+		setShowResult(false);
+		setScore(0);
+		localStorage.removeItem('hpQuizScore');
+	};
+
+	if (!quizStarted) {
+		return (
+			<div className="quiz-start">
+				<button onClick={() => setQuizStarted(true)} className="start-btn">
+					Start Harry Potter Spells Quiz
+				</button>
+			</div>
+		);
+	}
+	//Rendering result when quiz is over
+	if (showResult) {
+		return (
+			<div className="quiz-result">
+				<h2>
+					Your score: {score} / {answers.length}
+				</h2>
+				{score === answers.length && <h2>You&apos;re really big fun</h2>}
+				{score >= answers.length - 2 && score < answers.length && (
+					<h2>Almost perfect! Great job!</h2>
+				)}
+				{score < answers.length - 2 && <h2>Keep practicing your spells!</h2>}
+				{score === 0 && <h2>You need to read</h2>}
+				<button onClick={resetQuiz}>Play Again</button>
+			</div>
+		);
+	}
+
+	return (
+		<div className="quiz-container">
+			{answers.length > 0 && answers[currentQuestion] ? (
+				<>
+					<h3>{answers[currentQuestion].question}</h3>
+					<div className="options">
+						{answers[currentQuestion].answers.map((option) => {
+							const isCorrect =
+								option.index === answers[currentQuestion].correct;
+							const isSelected = selected === option.index;
+							const isOther = selected !== null && !isSelected;
+
+							return (
+								//Animate buttons after pushing
+								<motion.button
+									key={option.text}
+									className="quiz-option"
+									initial={{ x: 0, opacity: 1 }}
+									animate={
+										isSelected
+											? {
+													x: 200,
+													backgroundColor: isCorrect ? '#4caf50' : '#f44336',
+													opacity: 1,
+											  }
+											: isOther
+											? { opacity: 0, scale: 0.9 }
+											: { x: 0, opacity: 1, scale: 1 }
+									}
+									transition={{ duration: 0.4 }}
+									onTap={() => handleAnswer(option.index)}
+								>
+									{option.text} {isSelected && (isCorrect ? '✅' : '❌')}
+								</motion.button>
+							);
+						})}
+					</div>
+				</>
+			) : (
+				<p>Loading...</p>
+			)}
+		</div>
+	);
+}
